ea8b9ed73a48313a5e784219c5a6940d56b76a1d
Daniel <daniel.bueno@correounivalle.edu.co>
2021-04-14 18:26:57 -0500

    Estructura y configuración inicial

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: {
+        index: '/src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/assets/',
+    },
+    resolve: {
+        extensions: [
+        '.js',
+        '.jsx'
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+          template: './public/index.html',
+          filename: './index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: './assets/[name].css',
+          })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
+    module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+            },
+          },
+          {
+            test: /\.html$/,
+            use: [
+              {
+                loader: 'html-loader',
+              },
+            ],
+          },
+          {
+            test: /\.css|.styl$/,
+            use: [
+              {
+                loader: MiniCssExtractPlugin.loader,
+              },
+              'css-loader',
+              'stylus-loader',
+            ],
+          },
+        ],
+      },
+}   
