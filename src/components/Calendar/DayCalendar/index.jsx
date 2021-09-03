558881dfd7cfcc3334b017a7a21e0283435dd936
Daniel <daniel.bueno@correounivalle.edu.co>
2021-09-02 19:57:40 -0500

    refactor daycalendar view

--- src/components/Calendar/DayCalendar/index.jsx
+++ src/components/Calendar/DayCalendar/index.jsx
@@ -6,6 +6,62 @@ import { beforeToday, isToday, isSelected } from '../utils'
 import { useToggleModal } from '../../../hooks/useToggleModal'
 import { Container, Week, Day, DateNumber, DayContainer, WeekDays, DayName, WeekDayContainer } from './styles'
 
+
+
+
+const DayHeader = () => (
+	<WeekDays >
+		{
+			['LU', 'MA', 'MI', 'JU', 'VI', 'SA', 'DO'].map(day => (
+				<WeekDayContainer key={day}>
+					<DayName >
+						{day}
+					</DayName>
+
+				</WeekDayContainer>
+			))
+		}
+	</WeekDays>
+)
+
+const CalendarWeek = ({ i, showModal, week, date }) => (
+	<Week key={i}>
+		{
+			week.map(day => (
+				<CalendarDay
+					date={date}
+					key={day.format('D').toString()}
+					showModal={showModal}
+					day={day} />
+
+			))
+		}
+	</Week>
+)
+
+const CalendarDay = ({ day, date, showModal }) => (
+	<DayContainer
+		selected={isSelected(day, date)}
+		before={beforeToday(day)}
+		today={isToday(day)}
+		onClick={() => showModal(day)}
+		key={day.format('D').toString()}>
+		<Day >
+			<DateNumber>
+				{
+					day.format('D').toString()
+				}
+			</DateNumber>
+			{
+				isToday(day) ?
+					<CalendarEventSummary />
+					: null
+
+			}
+		</Day>
+	</DayContainer>
+)
+
 const DayCalendar = props => {
 	const { date, setDate, calendar, setCalendar, view, setView } = props,
 		{ showModal, hideModal, visible, modalRef, backgroundClick, data } = useToggleModal()
@@ -13,46 +69,10 @@ const DayCalendar = props => {
 	return (
 		<Container>
 			<Header setDate={setDate} date={date} view={view} setView={setView} setCalendar={setCalendar} />
-			<WeekDays >
-				{
-					['LU', 'MA', 'MI', 'JU', 'VI', 'SA', 'DO'].map(day => (
-						<WeekDayContainer key={day}>
-							<DayName >
-								{day}
-							</DayName>
-
-						</WeekDayContainer>
-					))
-				}
-			</WeekDays>
+			<DayHeader />
 			{
 				calendar[0].map && calendar.map((week, i) => (
-					<Week key={i}>
-						{
-							week.map(day => (
-								<DayContainer
-									selected={isSelected(day, date)}
-									before={beforeToday(day)}
-									today={isToday(day)}
-									onClick={() => showModal(day)}
-									key={day.format('D').toString()}>
-									<Day >
-										<DateNumber>
-											{
-												day.format('D').toString()
-											}
-										</DateNumber>
-										{
-											isToday(day) ?
-												<CalendarEventSummary />
-												: null
-
-										}
-									</Day>
-								</DayContainer>
-							))
-						}
-					</Week>
+					<CalendarWeek key={i} week={week} showModal={showModal} date={date} />
 				))
 			}
 			{
