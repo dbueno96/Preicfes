ef4f1e4c40ca21ef766d035764069ff24c7732f5
Daniel <daniel.bueno@correounivalle.edu.co>
2021-09-20 21:56:13 -0500

    Servicio para consultar eventos de calendario

--- src/services/index.js
+++ src/services/index.js
@@ -1,17 +1,17 @@
 import axios from 'axios'
 
-const baseURL = 'http://localhost:3000'
+const BASE_URL = 'http://localhost:3000'
 
 
 export const getInitialState = () => {
-	const res = axios.get(`${baseURL}/initialState`)
+	const res = axios.get(`${BASE_URL}/initialState`)
 	return res
 		.then(res => res.data)
 		.catch(() => console.error('Response error for initialState'))
 }
 
 export const fetchListData = entity => {
-	const res = axios.get(`${baseURL}/${entity}`)
+	const res = axios.get(`${BASE_URL}/${entity}`)
 	return res
 		.then(res => res.data)
 		.catch(() => console.error(`Response error for ${entity}`))
@@ -19,9 +19,16 @@ export const fetchListData = entity => {
 }
 
 export const fetchFormConfig = form => {
-	const res = axios.get(`${baseURL}/${form}`)
+	const res = axios.get(`${BASE_URL}/${form}`)
 	return res
 		.then(res => res.data)
 		.catch(() => console.error(`Response error for form ${form}`))
 
+}
+
+export const fetchScheduledEvents = date => {
+	const res = axios.get(`${BASE_URL}/events`)
+	return res
+		.then(res => res.data)
+		.catch(() => console.error(`Response error for events at ${date.format('MM-YYYY')}`))
 }
